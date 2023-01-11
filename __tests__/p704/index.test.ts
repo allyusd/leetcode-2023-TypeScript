404b1e62063fe3e860cc39f4b3f104a113b6431b
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-11 17:06:01 -0500

    704. Binary Search - Accepted

--- __tests__/p704/index.test.ts
+++ __tests__/p704/index.test.ts
@@ -4,6 +4,14 @@ describe("704. Binary Search", () => {
   test.each([
     [[-1, 0, 3, 5, 9, 12], 9, 4],
     [[-1, 0, 3, 5, 9, 12], 2, -1],
+    // 空陣列
+    [[], 2, -1],
+    // 第一個
+    [[-1, 0, 3, 5, 9, 12], -1, 0],
+    // 最後一個
+    [[-1, 0, 3, 5, 9, 12], 12, 5],
+    // 正中間
+    [[-1, 0, 3, 5, 9], 3, 2],
   ])("case", (nums, target, expected) => {
     const result = exportWarp(nums, target);
     expect(result).toStrictEqual(expected);
--- src/p704/index.ts
+++ src/p704/index.ts
@@ -4,6 +4,37 @@ export function exportWarp(nums: number[], target: number): number {
 
 // 2023-01-11
 // 704. Binary Search
-function search(nums: number[], target: number): number {
+// Constraints:
+// 1 <= nums.length <= 104
+// -104 < nums[i], target < 104
+// All the integers in nums are unique.
+// nums is sorted in ascending order.
+function search(nums: number[], target: number, preIndex = 0): number {
+  // 因為排序過，
+  // 所以從中間開始找，等於就是答案，
+  // 小於就用右邊的陣列，大於就用左邊的陣列。
+  // 左右陣列都空的就是沒有
+  const mid = Math.floor(nums.length / 2);
+  // console.log(`mid ${mid}`);
+
+  if (nums[mid] === target) {
+    // console.log(`=== ${mid}`);
+
+    return mid + preIndex;
+  }
+
+  if (nums[mid] < target) {
+    // console.log(`< ${mid}`);
+
+    // slice 包含前者，不包含後者
+    return search(nums.slice(mid + 1, nums.length), target, mid + 1 + preIndex);
+  }
+
+  if (nums[mid] > target) {
+    // console.log(`> ${mid}`);
+
+    return search(nums.slice(0, mid), target, preIndex);
+  }
+
   return -1;
 }
