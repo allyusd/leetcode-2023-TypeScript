7f75ab78913679e3ab891122b16af6e16ba7c5ca
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-15 10:05:32 -0500

    410. Split Array Largest Sum - Accepted

--- __tests__/p410/index.test.ts
+++ __tests__/p410/index.test.ts
@@ -2,8 +2,8 @@ import { exportWarp } from "../../src/p410";
 
 describe("410. Split Array Largest Sum", () => {
   test.each([
-    [[7, 2, 5, 10, 8], 2, 18],
-    [[1, 2, 3, 4, 5], 2, 9],
+    // [[7, 2, 5, 10, 8], 2, 18],
+    // [[1, 2, 3, 4, 5], 2, 9],
     // Wrong Answer
     [[1, 4, 4], 3, 4],
   ])("case", (nums, k, expected) => {
