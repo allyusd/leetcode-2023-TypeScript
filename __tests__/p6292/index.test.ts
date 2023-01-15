f93dac874a6061e1e3af3fcde4f367cb04b3a7a6
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-14 22:50:08 -0500

    6292. Increment Submatrices by One - Wrong Answer

--- __tests__/p6292/index.test.ts
+++ __tests__/p6292/index.test.ts
@@ -22,6 +22,16 @@ describe("6292. Increment Submatrices by One", () => {
         [1, 1],
       ],
     ],
+    // x, y 不對稱
+    [
+      3,
+      [[0, 0, 2, 0]],
+      [
+        [1, 1, 1],
+        [0, 0, 0],
+        [0, 0, 0],
+      ],
+    ],
   ])("case", (n, queries, expected) => {
     const result = exportWarp(n, queries);
     expect(result).toStrictEqual(expected);
--- src/p6292/index.ts
+++ src/p6292/index.ts
@@ -2,6 +2,19 @@ export function exportWarp(n: number, queries: number[][]): number[][] {
   return rangeAddQueries(n, queries);
 }
 
+function initialMatrix (size: number): number[][] {
+  const matrix: number[][] = [];
+
+  for (let y = 0; y < size; y++) {
+    matrix[y] = [];
+    for (let x = 0; x < size; x++) {
+      matrix[y][x] = 0;
+    }
+  }
+
+  return matrix;
+};
+
 // 2023-01-14
 // 6292. Increment Submatrices by One
 // Constraints:
@@ -10,7 +23,15 @@ export function exportWarp(n: number, queries: number[][]): number[][] {
 // 0 <= row1i <= row2i < n
 // 0 <= col1i <= col2i < n
 function rangeAddQueries(n: number, queries: number[][]): number[][] {
-  const output: number[][] = [];
+  const matrix: number[][] = initialMatrix(n);
 
-  return output;
+  queries.forEach((q: number[]) => {
+    for (let y = q[1]; y <= q[3]; y++) {
+      for (let x = q[0]; x <= q[2]; x++) {
+        matrix[y][x]++;
+      }
+    }
+  });
+
+  return matrix;
 }
