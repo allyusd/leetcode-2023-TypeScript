2607a814e26ebcc8f9e9cc1ab59d22617f8acb8f
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-15 14:14:14 -0500

    143. Reorder List -  題目建立

---
+++ __tests__/p143/index.test.ts
@@ -0,0 +1,22 @@
+import { exportWarp, ListNode } from "../../src/p143";
+
+describe("143. Reorder List", () => {
+  test.each([
+    [
+      [1, 2, 3, 4],
+      [1, 4, 2, 3],
+    ],
+    [
+      [1, 2, 3, 4, 5],
+      [1, 5, 2, 4, 3],
+    ],
+  ])("case", (head, expected) => {
+    const node = ListNode.fromArray(head);
+    // console.log(node);
+    // console.log(ListNode.toArray(node));
+    exportWarp(node);
+    const array = ListNode.toArray(node);
+    // console.log(array);
+    expect(array).toStrictEqual(expected);
+  });
+});
---
+++ src/p143/index.ts
@@ -0,0 +1,86 @@
+export function exportWarp(head: ListNode | null): void {
+  return reorderList(head);
+}
+
+//  * Definition for singly-linked list.
+export class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+
+  static fromArray(array: number[]): ListNode | null {
+    if (array.length === 0) {
+      return null;
+    }
+
+    const head = new ListNode(array[0]);
+    let preNode = head;
+
+    for (let i = 1; i < array.length; i++) {
+      const node = new ListNode(array[i]);
+      preNode.next = node;
+      preNode = node;
+    }
+
+    return head;
+  }
+
+  static fromArrayWithCycle(array: number[], pos: number): ListNode | null {
+    if (array.length === 0) {
+      return null;
+    }
+
+    const nodeArray: ListNode[] = [];
+
+    const head = new ListNode(array[0]);
+    let preNode = head;
+    nodeArray.push(head);
+
+    for (let i = 1; i < array.length; i++) {
+      const node = new ListNode(array[i]);
+      preNode.next = node;
+      preNode = node;
+      nodeArray.push(preNode);
+    }
+
+    if (pos >= 0 && pos < array.length) {
+      preNode.next = nodeArray[pos];
+    }
+
+    return head;
+  }
+
+  static toArray(head: ListNode | null): number[] {
+    const array = [];
+    let node = head;
+
+    while (node !== null) {
+      array.push(node.val);
+      node = node.next;
+    }
+
+    return array;
+  }
+}
+
+// 2023-01-15
+// 143. Reorder List
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+/**
+ Do not return anything, modify head in-place instead.
+ */
+function reorderList(head: ListNode | null): void {}
