e8878ef50f415de90d3fffe4cfc80fcd2b905225
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-15 11:48:01 -0500

    206. Reverse Linked List - Accepted

--- __tests__/p206/index.test.ts
+++ __tests__/p206/index.test.ts
@@ -3,17 +3,14 @@ import { exportWarp, ListNode } from "../../src/p206";
 describe("206. Reverse Linked List", () => {
   test.each([
     [
-      [1,2,3,4,5],
-      [5,4,3,2,1],
+      [1, 2, 3, 4, 5],
+      [5, 4, 3, 2, 1],
     ],
-    // [
-    //   [1,2],
-    //   [2,1],
-    // ],
-    // [
-    //   [],
-    //   [],
-    // ],
+    [
+      [1, 2],
+      [2, 1],
+    ],
+    [[], []],
   ])("case", (head, expected) => {
     const node = ListNode.fromArray(head);
     // console.log(node);
--- src/p206/index.ts
+++ src/p206/index.ts
@@ -4,45 +4,46 @@ export function exportWarp(head: ListNode | null): ListNode | null {
 
 //  * Definition for singly-linked list.
 export class ListNode {
-    val: number
-    next: ListNode | null
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.next = (next===undefined ? null : next)
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+
+  static fromArray(array: number[]): ListNode | null {
+    if (array.length === 0) {
+      return null;
     }
 
-    static fromArray(array: number[]): ListNode | null {
-      if (array.length === 0) {
-        return null;
-      }
+    const head = new ListNode(array[0]);
+    let preNode = head;
 
-      const head = new ListNode(array[0]);
-      let preNode = head;
-
-      for (let i = 1; i < array.length; i++) {
-        let node = new ListNode(array[i]);
-        preNode.next = node;
-        preNode = node;
-      }
-
-      return head;
-    };
-
-    static toArray(head: ListNode | null): number[] {
-      let array = [];
-      let node = head;
-
-      while(node!== null) {
-        array.push(node.val);
-        node = node.next;
-      }
-
-      return array;
+    for (let i = 1; i < array.length; i++) {
+      const node = new ListNode(array[i]);
+      preNode.next = node;
+      preNode = node;
     }
+
+    return head;
+  }
+
+  static toArray(head: ListNode | null): number[] {
+    const array = [];
+    let node = head;
+
+    while (node !== null) {
+      array.push(node.val);
+      node = node.next;
+    }
+
+    return array;
+  }
 }
 
 // 2023-01-15
 // 206. Reverse Linked List
+// Accepted
 // Constraints:
 // The number of nodes in the list is the range [0, 5000].
 // -5000 <= Node.val <= 5000
@@ -59,5 +60,29 @@ export class ListNode {
  */
 
 function reverseList(head: ListNode | null): ListNode | null {
-  return null;
-};
+  const stack: ListNode[] = [];
+
+  let node: ListNode | null = head;
+  while (node !== null) {
+    stack.push(node);
+    node = node.next;
+  }
+
+  const newHead = stack.pop();
+  if (newHead === null || newHead === undefined) {
+    return null;
+  }
+
+  let preNode: ListNode = newHead;
+  while (stack.length > 0) {
+    console.log(`${stack.length}`);
+    const node = stack.pop();
+    if (node !== undefined) {
+      preNode.next = node;
+      preNode = node;
+    }
+  }
+  preNode.next = null;
+
+  return newHead;
+}
