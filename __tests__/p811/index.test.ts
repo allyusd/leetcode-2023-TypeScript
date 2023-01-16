bf198adccb24b7c4d3a70cbfa37c8b159c12b275
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-16 00:20:38 -0500

    811. Subdomain Visit Count - 題目建立

---
+++ __tests__/p811/index.test.ts
@@ -0,0 +1,14 @@
+import { exportWarp } from "../../src/p811";
+
+describe("811. Subdomain Visit Count", () => {
+  test.each([
+    [["9001 discuss.leetcode.com"], ["9001 leetcode.com", "9001 discuss.leetcode.com", "9001 com"]],
+    [
+      ["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"],
+      ["901 mail.com", "50 yahoo.com", "900 google.mail.com", "5 wiki.org", "5 org", "1 intel.mail.com", "951 com"],
+    ],
+  ])("case", (cpdomains, expected) => {
+    const result = exportWarp(cpdomains);
+    expect(result).toStrictEqual(expected);
+  });
+});
---
+++ src/p811/index.ts
@@ -0,0 +1,9 @@
+export function exportWarp(cpdomains: string[]): string[] {
+  return subdomainVisits(cpdomains);
+}
+
+// 2023-01-15
+// 811. Subdomain Visit Count
+function subdomainVisits(cpdomains: string[]): string[] {
+  return [];
+}
