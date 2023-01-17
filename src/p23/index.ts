6b97703bf1bd7630b2ad3b3a6da4cd729c6ffe11
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-17 14:40:58 -0500

    23. Merge k Sorted Lists - Wrong Answer

--- src/p23/index.ts
+++ src/p23/index.ts
@@ -53,8 +53,54 @@ export class ListNode {
   }
 }
 
+// 避免同名獨立出來
+class Converter {
+  static fromArray(array: number[]): ListNode | null {
+    if (array.length === 0) {
+      return null;
+    }
+    const input = [...array];
+
+    const first = input.shift();
+
+    const head = new ListNode(first);
+    let preNode = head;
+
+    while (input.length > 0) {
+      const val = input.shift();
+      if (val === undefined) {
+        throw new Error("Here is undefined");
+      }
+
+      const node = new ListNode(val);
+      preNode.next = node;
+      preNode = node;
+    }
+
+    return head;
+  }
+
+  static toArray(head: ListNode | null): number[] {
+    if (head === null) {
+      return [];
+    }
+
+    const array = [];
+    array.push(head.val);
+    let next: ListNode = head;
+    while (next.next !== null) {
+      next = next.next;
+      array.push(next.val);
+    }
+
+    return array;
+  }
+}
+
 // 2023-01-17
 // 23. Merge k Sorted Lists
+// 看到這一題直覺得想法，但是不確定這種把 Linked List 轉成 Array，
+// 處理之後再轉回 Linked List 的做法是否有違反他的題意
 /**
  * Definition for singly-linked list.
  * class ListNode {
@@ -68,5 +114,17 @@ export class ListNode {
  */
 
 function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
-  return null;
+  // 把 linked list 轉成 array 並且 合併
+  let numberArray:number[] = [];
+  for (let list of lists) {
+    if (list !== null) {
+      numberArray = numberArray.concat(Converter.toArray(list));
+    }
+  }
+
+  // 排序
+  numberArray.sort();
+
+  // 把 array 轉回 Linked list
+  return Converter.fromArray(numberArray);
 };
