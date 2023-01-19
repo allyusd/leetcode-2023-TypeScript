bc9d29c816ef8e4e065b5a76d8f8703ab24d1555
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-19 13:02:29 -0500

    733. Flood Fill - Accepted

--- __tests__/p733/index.test.ts
+++ __tests__/p733/index.test.ts
@@ -30,6 +30,22 @@ describe("733. Flood Fill", () => {
         [0, 0, 0],
       ],
     ],
+    // x, y 非對稱
+    [
+      [
+        [1, 1, 1, 1],
+        [1, 1, 0, 1],
+        [1, 0, 1, 1],
+      ],
+      1,
+      3,
+      2,
+      [
+        [2, 2, 2, 2],
+        [2, 2, 0, 2],
+        [2, 0, 2, 2],
+      ],
+    ],
   ])("case", (image, sr, sc, color, expected) => {
     const result = exportWarp(image, sr, sc, color);
     expect(result).toStrictEqual(expected);
--- src/p733/index.ts
+++ src/p733/index.ts
@@ -5,5 +5,30 @@ export function exportWarp(image: number[][], sr: number, sc: number, color: num
 // 2023-01-19
 // 733. Flood Fill
 function floodFill(image: number[][], sr: number, sc: number, color: number): number[][] {
+  let target = image[sr][sc];
+
+  if (target === color) {
+    return image;
+  }
+
+  const fillColor = (sr: number, sc: number, target: number, color: number) => {
+    if (sr >= 0 && sr < image.length
+      && sc >= 0 && sc < image[0].length) {
+      if (image[sr][sc] === target) {
+        image[sr][sc] = color;
+        autoFill(sr, sc, target, color);
+      }
+    }
+  };
+
+  const autoFill = (sr: number, sc: number, target: number, color: number) => {
+    fillColor(sr - 1, sc, target, color);
+    fillColor(sr, sc + 1, target, color);
+    fillColor(sr + 1, sc, target, color);
+    fillColor(sr, sc - 1, target, color);
+  }
+
+  fillColor(sr, sc, target, color);
+
   return image;
 }
