8d8d902ab40b9416b7b80d557d5330f304ef1247
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-19 13:02:54 -0500

    lint

--- src/p733/index.ts
+++ src/p733/index.ts
@@ -5,15 +5,14 @@ export function exportWarp(image: number[][], sr: number, sc: number, color: num
 // 2023-01-19
 // 733. Flood Fill
 function floodFill(image: number[][], sr: number, sc: number, color: number): number[][] {
-  let target = image[sr][sc];
+  const target = image[sr][sc];
 
   if (target === color) {
     return image;
   }
 
   const fillColor = (sr: number, sc: number, target: number, color: number) => {
-    if (sr >= 0 && sr < image.length
-      && sc >= 0 && sc < image[0].length) {
+    if (sr >= 0 && sr < image.length && sc >= 0 && sc < image[0].length) {
       if (image[sr][sc] === target) {
         image[sr][sc] = color;
         autoFill(sr, sc, target, color);
@@ -26,7 +25,7 @@ function floodFill(image: number[][], sr: number, sc: number, color: number): nu
     fillColor(sr, sc + 1, target, color);
     fillColor(sr + 1, sc, target, color);
     fillColor(sr, sc - 1, target, color);
-  }
+  };
 
   fillColor(sr, sc, target, color);
 
