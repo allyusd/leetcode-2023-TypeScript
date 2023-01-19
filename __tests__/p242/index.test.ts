8b6c3e9dee41cc39956a424f2922c2ce9b2b604c
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-18 23:25:03 -0500

    242. Valid Anagram - Accepted

--- __tests__/p242/index.test.ts
+++ __tests__/p242/index.test.ts
@@ -4,6 +4,10 @@ describe("242. Valid Anagram", () => {
   test.each([
     ["anagram", "nagaram", true],
     ["rat", "car", false],
+    // 長度不同的話
+    ["abc", "a", false],
+    // 只有一個字母的話
+    ["a", "a", true],
   ])("case", (s, t, expected) => {
     const result = exportWarp(s, t);
     expect(result).toStrictEqual(expected);
--- src/p242/index.ts
+++ src/p242/index.ts
@@ -5,5 +5,19 @@ export function exportWarp(s: string, t: string): boolean {
 // 2023-01-18
 // 242. Valid Anagram
 function isAnagram(s: string, t: string): boolean {
-  return false;
+  // 長度不同就不可能
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  for (const c of s) {
+    const index = t.indexOf(c);
+    if (index === -1) {
+      return false;
+    }
+
+    t = t.substring(0, index) + t.substring(index + 1);
+  }
+
+  return true;
 }
