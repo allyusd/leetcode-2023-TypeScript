d528279753625625d3eabe2d978d9ba24fcd9d4b
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-21 09:40:55 -0500

    6300. Minimum Common Value - Time Limit Exceeded

--- __tests__/p6300/index.test.ts
+++ __tests__/p6300/index.test.ts
@@ -4,6 +4,8 @@ describe("6300. Minimum Common Value", () => {
   test.each([
     [[1, 2, 3], [2, 4], 2],
     [[1, 2, 3, 6], [2, 3, 4, 5], 2],
+    // first
+    [[2, 3, 6], [2, 3, 4, 5], 2],
   ])("case %#", (nums1, nums2, expected) => {
     const result = exportWarp(nums1, nums2);
     expect(result).toStrictEqual(expected);
--- src/p6300/index.ts
+++ src/p6300/index.ts
@@ -5,5 +5,19 @@ export function exportWarp(nums1: number[], nums2: number[]): number {
 // 2023-01-21
 // 6300. Minimum Common Value
 function getCommon(nums1: number[], nums2: number[]): number {
+  while (nums1.length > 0 && nums2.length > 0) {
+    if (nums1[0] < nums2[0]) {
+      if (nums2.includes(nums1[0])) {
+        return nums1[0];
+      }
+      nums1.shift();
+    } else {
+      if (nums1.includes(nums2[0])) {
+        return nums2[0];
+      }
+      nums2.shift();
+    }
+  }
+
   return -1;
 }
