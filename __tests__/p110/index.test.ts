c3021513e3f7627ebca8bb40690480965e54de73
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-21 09:00:22 -0500

    110. Balanced Binary Tree - create question

---
+++ __tests__/p110/index.test.ts
@@ -0,0 +1,19 @@
+import { exportWarp } from "../../src/p110";
+import { TreeHelper } from "../../src/p110/TreeHelper";
+
+describe("110. Balanced Binary Tree", () => {
+  test.each([
+    [[3, 9, 20, null, null, 15, 7], true],
+    [[1, 2, 2, 3, 3, null, null, 4, 4], false],
+    [[], true],
+  ])("case %#", (root, expected) => {
+    // helper test
+    expect(root).toStrictEqual(TreeHelper.toArray(TreeHelper.fromArray(root)));
+
+    const tree = TreeHelper.fromArray(root);
+    const result = exportWarp(tree);
+    // const array = TreeHelper.toArray(result);
+
+    expect(result).toStrictEqual(expected);
+  });
+});
---
+++ src/p110/index.ts
@@ -0,0 +1,11 @@
+import { TreeNode } from "./TreeNode";
+
+export function exportWarp(root: TreeNode | null): boolean {
+  return isBalanced(root);
+}
+
+// 2023-01-21
+// 110. Balanced Binary Tree
+function isBalanced(root: TreeNode | null): boolean {
+  return false;
+}
