9324a833061cbebe3212d0d1d585e105ca049b78
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-21 21:49:32 -0500

    6297. Sort the Students by Their Kth Score - Accepted

--- src/p6297/index.ts
+++ src/p6297/index.ts
@@ -5,5 +5,11 @@ export function exportWarp(score: number[][], k: number): number[][] {
 // 2023-01-21
 // 6297. Sort the Students by Their Kth Score
 function sortTheStudents(score: number[][], k: number): number[][] {
+  score.sort((a, b) => {
+    if (a[k] > b[k]) return -1;
+    if (a[k] < b[k]) return 1;
+    return 0;
+  });
+
   return score;
 }
