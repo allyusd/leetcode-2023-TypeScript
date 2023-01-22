ef5aa2e3b095872719033790d9c8f2ca4d8a5a92
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-21 21:38:43 -0500

    6296. Alternating Digit Sum - Accepted

--- src/p6296/index.ts
+++ src/p6296/index.ts
@@ -5,5 +5,17 @@ export function exportWarp(n: number): number {
 // 2023-01-21
 // 6296. Alternating Digit Sum
 function alternateDigitSum(n: number): number {
-  return n;
+  const str = n.toString();
+    // console.log(str);
+
+  let sign = 1;
+
+  let sum = 0;
+  for (let i = 0; i < str.length; i++) {
+    let digit = Number(str[i]);
+    sum += digit * sign;
+    sign *= -1;
+  }
+
+  return sum;
 }
