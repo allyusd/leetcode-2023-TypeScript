a1ae123dc93ec18da90259fb26423167d4a82f0e
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-21 21:32:30 -0500

    6296. Alternating Digit Sum - Create Question

---
+++ __tests__/p6296/index.test.ts
@@ -0,0 +1,12 @@
+import { exportWarp } from "../../src/p6296";
+
+describe("6296. Alternating Digit Sum", () => {
+  test.each([
+    [521, 4],
+    [111, 1],
+    [886996, 0],
+  ])("case %#", (n, expected) => {
+    const result = exportWarp(n);
+    expect(result).toStrictEqual(expected);
+  });
+});
---
+++ src/p6296/index.ts
@@ -0,0 +1,9 @@
+export function exportWarp(n: number): number {
+  return alternateDigitSum(n);
+}
+
+// 2023-01-21
+// 6296. Alternating Digit Sum
+function alternateDigitSum(n: number): number {
+  return n;
+}
