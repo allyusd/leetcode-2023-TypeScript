70c87962f6c05be58f86a671f84ad4edb6137b2a
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-21 22:37:10 -0500

    6298. Apply Bitwise Operations to Make Strings Equal - Wrong Answer

--- __tests__/p6298/index.test.ts
+++ __tests__/p6298/index.test.ts
@@ -4,6 +4,13 @@ describe("6298. Apply Bitwise Operations to Make Strings Equal", () => {
   test.each([
     ["1010", "0110", true],
     ["11", "00", false],
+    // 自己想的
+    ["10", "00", false],
+    ["10", "01", true],
+    ["1010", "0100", true],
+    ["1010", "0000", false],
+    // Wrong Answer
+    ["00", "10", false],
   ])("case %#", (s, target, expected) => {
     const result = exportWarp(s, target);
     expect(result).toStrictEqual(expected);
--- src/p6298/index.ts
+++ src/p6298/index.ts
@@ -5,5 +5,19 @@ export function exportWarp(s: string, target: string): boolean {
 // 2023-01-21
 // 6298. Apply Bitwise Operations to Make Strings Equal
 function makeStringsEqual(s: string, target: string): boolean {
-  return false;
+  return target.includes("1");
 }
+
+// 0 OR 0 = 0
+// 1 OR 0 = 1
+// 0 OR 1 = 1
+// 1 OR 1 = 1
+
+// 0 XOR 0 = 0
+// 1 XOR 0 = 1
+// 0 XOR 1 = 1
+// 1 XOR 1 = 0
+
+// 如果有 1 的話，可以把另一個數字改成另一個數字
+// 如果是 0 的話，自己改變，對方不變
+// 所以都是改一個數字嗎？
