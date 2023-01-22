db2f2decbc5e0939813d096e32cd8d0e15c92859
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-21 23:03:29 -0500

    6299. Minimum Cost to Split an Array - The contest has ended.

--- src/p6299/index.ts
+++ src/p6299/index.ts
@@ -5,5 +5,14 @@ export function exportWarp(nums: number[], k: number): number {
 // 2023-01-21
 // 6299. Minimum Cost to Split an Array
 function minCost(nums: number[], k: number): number {
+  const trimmed = (nums: number[]): number[] => {
+    return nums.filter(n => {
+      const index = nums.indexOf(n);
+      const lastIndex = nums.lastIndexOf(n);
+
+      return index !== lastIndex;
+    })
+  };
+
   return -1;
 }
