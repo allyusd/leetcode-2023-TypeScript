e537c299556d702125c987e401d75adabeb708a2
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-28 21:42:49 -0500

    6337. Count Distinct Numbers on Board - Accepted

--- __tests__/p6337/index.test.ts
+++ __tests__/p6337/index.test.ts
@@ -4,6 +4,9 @@ describe("6337. Count Distinct Numbers on Board", () => {
   test.each([
     [5, 4],
     [3, 2],
+    // 自己加
+    [100, 99],
+    [1, 1],
   ])("case %#", (n, expected) => {
     const result = exportWarp(n);
 
--- src/p6337/index.ts
+++ src/p6337/index.ts
@@ -5,5 +5,19 @@ export function exportWarp(n: number): number {
 // 2023-01-28
 // 6337. Count Distinct Numbers on Board
 function distinctIntegers(n: number): number {
-  return n;
+  const board: number[] = [n];
+  let flag = true;
+  while (flag) {
+    flag = false;
+    for (let i = 0; i < n; i++) {
+      for (const x of board) {
+        if (x % i === 1 && board.includes(i) === false) {
+          board.push(i);
+          flag = true;
+        }
+      }
+    }
+  }
+
+  return board.length;
 }
