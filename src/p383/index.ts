4839dd3669bdbe69b5c12982a749b68378bc8e0b
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-28 20:44:05 -0500

    383. Ransom Note - Accepted

--- src/p383/index.ts
+++ src/p383/index.ts
@@ -1,3 +1,5 @@
+import { StringHelper } from "./stringHelper";
+
 export function exportWarp(ransomNote: string, magazine: string): boolean {
   return canConstruct(ransomNote, magazine);
 }
@@ -5,5 +7,20 @@ export function exportWarp(ransomNote: string, magazine: string): boolean {
 // 2023-01-28
 // 383. Ransom Note
 function canConstruct(ransomNote: string, magazine: string): boolean {
-  return false;
+
+  while (ransomNote.length > 0) {
+    if (magazine.length === 0) {
+      return false;
+    }
+
+    const index = magazine.indexOf(ransomNote[0]);
+    if (index === -1) {
+      return false;
+    }
+
+    ransomNote = ransomNote.slice(1, ransomNote.length);
+    magazine = StringHelper.removeCharByIndex(magazine, index);
+  }
+
+  return true;
 }
