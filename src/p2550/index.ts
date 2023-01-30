93ba2e0b868461044cb20d0fa6740b1d2e3ad6a5
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-01-30 08:19:51 -0500

    lint

--- src/p2550/index.ts
+++ src/p2550/index.ts
@@ -20,7 +20,7 @@ class Vertex {
     } else {
       vertices[index]++;
     }
-    
+
     // console.log(`index: ${index}`);
     // console.log(`vertices: ${vertices}`);
   }
@@ -31,7 +31,7 @@ class Vertex {
     // console.log(vertices.filter(x => x > 1).length);
     // console.log(vertices.filter(x => x > 1).length > 1);
 
-    return vertices.filter(x => x > 1).length > 0;
+    return vertices.filter((x) => x > 1).length > 0;
   }
 }
 
@@ -39,7 +39,7 @@ class Vertex {
 // 6338. Count Collisions of Monkeys on a Polygon
 // 思考有誤，目前的寫法是以跳過去的頂點為紀錄，但是忽略了兩個同時交換時碰撞的情況
 function monkeyMove(n: number): number {
-  const fn = function(vertices: number[], i: number): number {
+  const fn = function (vertices: number[], i: number): number {
     const left = [...vertices];
     Vertex.add(left, i - 1);
     const right = [...vertices];
@@ -50,7 +50,7 @@ function monkeyMove(n: number): number {
     } else {
       return fn(left, i + 1) + fn(right, i + 1);
     }
-  }
+  };
 
   const array = Vertex.init(n);
 
--- src/p2551/index.ts
+++ src/p2551/index.ts
@@ -4,6 +4,7 @@ export function exportWarp(weights: number[], k: number): number {
 
 // 2023-01-28
 // 2551. Put Marbles in Bags
+// eslint-disable-next-line  @typescript-eslint/no-unused-vars
 function putMarbles(weights: number[], k: number): number {
   return 0;
 }
