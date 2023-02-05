b0556e4f8d46e709e780f2d6257b90dcc98ebafc
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-02-04 21:43:58 -0500

    6348. Take Gifts From the Richest Pile - Accepted

--- src/p6348/index.ts
+++ src/p6348/index.ts
@@ -5,5 +5,22 @@ export function exportWarp(gifts: number[], k: number): number {
 // 2023-02-04
 // 6348. Take Gifts From the Richest Pile
 function pickGifts(gifts: number[], k: number): number {
-  return k;
+  for (let i = 0; i < k; i++) {
+    const max = Math.max(...gifts);
+
+    if (max === 1) {
+      break;
+    }
+
+    // console.log(`max: ${max}`);
+    const maxIndex = gifts.indexOf(max);
+    const sqrt = Math.floor(Math.sqrt(gifts[maxIndex]));
+    // console.log(`sqrt: ${sqrt}`);
+    gifts[maxIndex] = sqrt;
+  }
+
+  // console.log(`gifts: ${gifts}`);
+  const sum = gifts.reduce((a, b) => a + b, 0);
+
+  return sum;
 }
