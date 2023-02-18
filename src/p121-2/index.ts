c7c97a2c4544ff40f629825e7901cbd42ace44dc
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-02-18 08:42:22 -0500

    121. Best Time to Buy and Sell Stock - Accepted

--- src/p121-2/index.ts
+++ src/p121-2/index.ts
@@ -5,5 +5,20 @@ export function exportWarp(prices: number[]): number {
 // 2023-02-18
 // 121. Best Time to Buy and Sell Stock
 function maxProfit(prices: number[]): number {
-  return prices[0];
+  let minPrice = prices[0];
+  let maxProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    const profit = prices[i] - minPrice;
+
+    if (profit > maxProfit) {
+      maxProfit = profit;
+    }
+
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    }
+  }
+
+  return maxProfit;
 }
