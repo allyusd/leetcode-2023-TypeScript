114cde061f337d96e084ffb18a70071a6f61b2e6
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-02-24 11:11:41 -0500

    1784. Check if Binary String Has at Most One Segment of Ones - Accepted

--- src/p1784/index.ts
+++ src/p1784/index.ts
@@ -5,5 +5,17 @@ export function exportWarp(s: string): boolean {
 // 2023-02-24
 // 1784. Check if Binary String Has at Most One Segment of Ones
 function checkOnesSegment(s: string): boolean {
-  return false;
+  let isBreak = false;
+
+  for(let c of s) {
+    if (isBreak == false && c === "0") {
+      isBreak = true;
+    }
+
+    if (isBreak == true && c === "1") {
+      return false;
+    }
+  }
+
+  return true;
 }
