7daf61d5a02414ea2e8b2e5bbb25916b69378016
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-02-25 22:08:57 -0500

    6367. Find the Maximum Number of Marked Indices - Create Question

---
+++ __tests__/p6367/index.test.ts
@@ -0,0 +1,12 @@
+import { exportWarp } from "../../src/p6367";
+
+describe("6367. Find the Maximum Number of Marked Indices", () => {
+  test.each([
+    [[3, 5, 2, 4], 2],
+    [[9, 2, 5, 4], 4],
+    [[7, 6, 8], 0],
+  ])("case", (nums, expected) => {
+    const result = exportWarp(nums);
+    expect(result).toStrictEqual(expected);
+  });
+});
---
+++ src/p6367/index.ts
@@ -0,0 +1,9 @@
+export function exportWarp(nums: number[]): number {
+  return maxNumOfMarkedIndices(nums);
+}
+
+// 2023-02-25
+// 6367. Find the Maximum Number of Marked Indices
+function maxNumOfMarkedIndices(nums: number[]): number {
+  return nums[0];
+}
