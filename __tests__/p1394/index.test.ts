442b8b7256924f1f86c00a2820e78f18563d92fe
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-02-25 21:45:27 -0500

    fix lint

--- __tests__/p1394/index.test.ts
+++ __tests__/p1394/index.test.ts
@@ -6,7 +6,7 @@ describe("1394. Find Lucky Integer in an Array", () => {
     [[1, 2, 2, 3, 3, 3], 3],
     [[2, 2, 2, 3, 3], -1],
     // Wrong Answer
-    [[4,3,2,2,4,1,3,4,3], 3],
+    [[4, 3, 2, 2, 4, 1, 3, 4, 3], 3],
   ])("case", (arr, expected) => {
     const result = exportWarp(arr);
     expect(result).toStrictEqual(expected);
--- src/p6369/index.ts
+++ src/p6369/index.ts
@@ -7,13 +7,13 @@ export function exportWarp(nums: number[]): number[] {
 function leftRigthDifference(nums: number[]): number[] {
   const sum = (arr: number[]) => {
     return arr.reduce((p, c) => p + c);
-  }
+  };
 
   const output: number[] = [];
   for (let i = 0; i < nums.length; i++) {
-    let leftSum = i == 0 ? 0 : sum(nums.slice(0, i));
-    let rightSum = i == nums.length - 1 ? 0 : sum(nums.slice(i + 1, nums.length));
-    let answer = Math.abs(leftSum - rightSum);
+    const leftSum = i == 0 ? 0 : sum(nums.slice(0, i));
+    const rightSum = i == nums.length - 1 ? 0 : sum(nums.slice(i + 1, nums.length));
+    const answer = Math.abs(leftSum - rightSum);
     output.push(answer);
   }
 
