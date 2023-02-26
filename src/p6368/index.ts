2a77b68fd2516461163c6d21d90be50d3c5027dd
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-02-25 21:59:43 -0500

    6368. Find the Divisibility Array of a String - Wrong Answer

--- src/p6368/index.ts
+++ src/p6368/index.ts
@@ -5,5 +5,13 @@ export function exportWarp(word: string, m: number): number[] {
 // 2023-02-25
 // 6368. Find the Divisibility Array of a String
 function divisibilityArray(word: string, m: number): number[] {
-  return [];
+  const output: number[] = [];
+
+  for (let i = 0; i < word.length; i++) {
+    const num = Number(word.slice(0, i + 1));
+    const divisibility = num % m === 0 ? 1 : 0;
+    output.push(divisibility);
+  }
+
+  return output;
 }
