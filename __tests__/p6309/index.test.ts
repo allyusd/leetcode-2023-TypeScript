d3bfaf575bdd819fac9b137c6b5623f92520538c
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-03-04 22:36:23 -0500

    6309. Split the Array to Make Coprime Products - Time Limit Exceeded

--- __tests__/p6309/index.test.ts
+++ __tests__/p6309/index.test.ts
@@ -1,9 +1,12 @@
 import { exportWarp } from "../../src/p6309";
+import { nums1 } from "./case1.data";
 
 describe("6309. Split the Array to Make Coprime Products", () => {
   test.each([
-    [[4, 7, 8, 15, 3, 5], 2],
-    [[4, 7, 15, 8, 3, 5], -1],
+    // [[4, 7, 8, 15, 3, 5], 2],
+    // [[4, 7, 15, 8, 3, 5], -1],
+    // Time Limit Exceeded
+    [nums1, -1],
   ])("case", (nums, expected) => {
     const result = exportWarp(nums);
     expect(result).toStrictEqual(expected);
--- src/p6309/index.ts
+++ src/p6309/index.ts
@@ -2,8 +2,40 @@ export function exportWarp(nums: number[]): number {
   return findValidSplit(nums);
 }
 
+// https://stackoverflow.com/a/17445322/2613884
+function gcd(a: number, b: number) {
+    a = Math.abs(a);
+    b = Math.abs(b);
+    if (b > a) {var temp = a; a = b; b = temp;}
+    while (true) {
+        if (b == 0) return a;
+        a %= b;
+        if (a == 0) return b;
+        b %= a;
+    }
+}
+
 // 2023-03-04
 // 6309. Split the Array to Make Coprime Products
 function findValidSplit(nums: number[]): number {
-  return nums[0];
+  const product = (arr: number[]): number => {
+    return arr.reduce((p, c) => p * c);
+  }
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    let pp = product(nums.slice(0, i + 1));
+    let sp = product(nums.slice(i + 1, nums.length));
+
+    // console.log(`pp: ${pp}`);
+    // console.log(`sp: ${sp}`);
+
+    let g = gcd(pp, sp);
+    // console.log(`g: ${g}`);
+
+    if (g === 1) {
+      return i;
+    }
+  }
+
+  return -1;
 }
