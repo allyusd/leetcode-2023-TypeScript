745c8ec8bae56e8580c02453aca5eaf7103a53e4
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-03-04 21:51:09 -0500

    6307. Pass the Pillow - Accepted

--- src/p6307/index.ts
+++ src/p6307/index.ts
@@ -5,5 +5,14 @@ export function exportWarp(n: number, time: number): number {
 // 2023-03-04
 // 6307. Pass the Pillow
 function passThePillow(n: number, time: number): number {
-  return n;
+  const m = n - 1;
+
+  const x = Math.floor(time / m);
+  const p = time % m;
+
+  if (x % 2 === 0) {
+    return 1 + p;
+  }
+
+  return n - p;
 }
