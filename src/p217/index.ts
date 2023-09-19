7351ba0ceb5cf8429654b75da62698f0dfbc7f82
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-09-19 19:28:04 +0800

    217. Contains Duplicate - Accepted

--- src/p217/index.ts
+++ src/p217/index.ts
@@ -1,6 +1,16 @@
 // 2023-09-19
+// Runtime Details 9579ms
+// Beats 5.03% of users with TypeScript
 export const title = "217. Contains Duplicate";
 
 export default function containsDuplicate(nums: number[]): boolean {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i !== j && nums[i] === nums[j]) {
+        return true;
+      }
+    }
+  }
+
   return false;
 }
