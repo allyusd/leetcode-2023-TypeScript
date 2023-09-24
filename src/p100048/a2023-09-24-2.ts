6833efd9bb096b58bc2333cdf52a6973194b92c7
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-09-24 12:01:00 +0800

    100048. Beautiful Towers II - 補上被覆蓋的版本

---
+++ src/p100048/a2023-09-24-2.ts
@@ -0,0 +1,51 @@
+export const title = "100048. Beautiful Towers II";
+
+// 2023-09-24
+// Wrong Answer
+export default function maximumSumOfHeights(maxHeights: number[]): number {
+  const beautiful = (maxHeights: number[], index: number): number => {
+    const output = [...maxHeights];
+    for (let i = index - 1; i >= 0; i--) {
+      if (output[i] > output[i + 1]) {
+        output[i] = output[i + 1];
+      }
+    }
+
+    for (let i = index + 1; i < maxHeights.length; i++) {
+      if (output[i] > output[i - 1]) {
+        output[i] = output[i - 1];
+      }
+    }
+
+    return output.reduce((p, c) => p + c);
+  };
+
+  let lmax = 0;
+  let rmax = maxHeights.length - 1;
+  for (let i = 0; i < maxHeights.length; i++) {
+    lmax = i;
+
+    if (maxHeights[i + 1] < maxHeights[i]) {
+      break;
+    }
+  }
+
+  for (let i = maxHeights.length - 1; i >= 0; i--) {
+    rmax = i;
+
+    if (maxHeights[i - 1] < maxHeights[i]) {
+      break;
+    }
+  }
+
+  let max = 0;
+
+  for (let i = lmax; i <= rmax; i++) {
+    const count = beautiful(maxHeights, i);
+    if (count > max) {
+      max = count;
+    }
+  }
+
+  return max;
+}
