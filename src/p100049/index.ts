b84f3ae20e91573ca1b900aa356596920959c98a
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-09-24 11:00:38 +0800

    100049. Beautiful Towers I - Accepted

--- src/p100049/index.ts
+++ src/p100049/index.ts
@@ -2,5 +2,31 @@ export const title = "100049. Beautiful Towers I";
 
 // 2023-09-24
 export default function maximumSumOfHeights(maxHeights: number[]): number {
-  return 0;
+  const beautiful = (maxHeights: number[], index: number): number => {
+    const output = [...maxHeights];
+    for (let i = index - 1; i >= 0; i--) {
+      if (output[i] > output[i + 1]) {
+        output[i] = output[i + 1];
+      }
+    }
+
+    for (let i = index + 1; i < maxHeights.length; i++) {
+      if (output[i] > output[i - 1]) {
+        output[i] = output[i - 1];
+      }
+    }
+
+    return output.reduce((p, c) => p + c);
+  };
+
+  let max = 0;
+
+  for (let i = 0; i < maxHeights.length; i++) {
+    const count = beautiful(maxHeights, i);
+    if (count > max) {
+      max = count;
+    }
+  }
+
+  return max;
 }
