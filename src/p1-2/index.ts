3a7fb2fda2cb2f0877501cc090faba4adb09eb19
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-09-27 14:59:25 +0800

    1. Two Sum - Accepted

--- src/p1-2/index.ts
+++ src/p1-2/index.ts
@@ -1,6 +1,16 @@
+import _ from "lodash";
+
 // 2023-09-27
 export const title = "1. Two Sum";
 
 export default function twoSum(nums: number[], target: number): number[] {
+  for (const i of _.range(nums.length)) {
+    for (const j of _.range(nums.length)) {
+      if (i !== j && nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+
   return [];
 }
