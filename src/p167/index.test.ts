f04055499cd0cce16669dd35a9c5f621b9ebb957
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-09-28 16:11:02 +0800

    167. Two Sum II - Input Array Is Sorted - Time Limit Exceeded

--- src/p167/index.test.ts
+++ src/p167/index.test.ts
@@ -1,4 +1,5 @@
 import solution, { title } from ".";
+import { input1 } from "./input1.data";
 
 interface CaseSet {
   name: string;
@@ -13,6 +14,7 @@ describe(title, () => {
     { name: "Example 1", data: { arg: [[2, 7, 11, 15], 9], expected: [1, 2] } },
     { name: "Example 2", data: { arg: [[2, 3, 4], 6], expected: [1, 3] } },
     { name: "Example 3", data: { arg: [[-1, 0], -1], expected: [1, 2] } },
+    { name: "Large 1", data: { arg: [input1, 2], expected: [29999, 30000] } },
   ])("$name", ({ data }) => {
     const result = solution(...data.arg);
     expect(result).toStrictEqual(data.expected);
--- src/p167/index.ts
+++ src/p167/index.ts
@@ -1,13 +1,14 @@
 import _ from "lodash";
 
 // 2023-09-28
+// Time Limit Exceeded
 export const title = "167. Two Sum II - Input Array Is Sorted";
 
-export default function twoSum(nums: number[], target: number): number[] {
-  for (const i of _.range(nums.length)) {
-    for (const j of _.range(nums.length)) {
-      if (i !== j && nums[i] + nums[j] === target) {
-        return [i, j];
+export default function twoSum(numbers: number[], target: number): number[] {
+  for (const i of _.range(numbers.length)) {
+    for (const j of _.range(numbers.length)) {
+      if (i !== j && numbers[i] + numbers[j] === target) {
+        return [i + 1, j + 1];
       }
     }
   }
