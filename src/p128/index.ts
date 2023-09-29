92da1dfe50d20b253b26c9f144c15130595298a7
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-09-29 10:36:35 +0800

    更新解題日期

--- src/p128/index.ts
+++ src/p128/index.ts
@@ -2,7 +2,7 @@ import _ from "lodash";
 
 export const title = "128. Longest Consecutive Sequence";
 
-// 2023-09-28
+// 2023-09-29
 // Time Limit Exceeded
 export default function longestConsecutive(nums: number[]): number {
   if (nums.length === 0) {
