be19d65a802f6f3e4672f0e8ae3a3ba728370cbe
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-09-29 11:07:58 +0800

    1299. Replace Elements with Greatest Element on Right Side - Accepted

--- src/p1299/index.ts
+++ src/p1299/index.ts
@@ -4,5 +4,18 @@ import _ from "lodash";
 export const title = "1299. Replace Elements with Greatest Element on Right Side";
 
 export default function replaceElements(arr: number[]): number[] {
-  return arr;
+  const output = [];
+  for (const i of _.range(arr.length - 1)) {
+    let max = arr[i + 1];
+    for (const j of _.range(i + 1, arr.length)) {
+      if (arr[j] > max) {
+        max = arr[j];
+      }
+    }
+    output.push(max);
+  }
+
+  output.push(-1);
+
+  return output;
 }
